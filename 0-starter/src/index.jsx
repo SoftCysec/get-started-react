ad8db06bcbdc3658a765cea31990c71fca56708a
SoftCysec <softengdedan@gmail.com>
2022-11-11 09:44:09 +0300

    React Initial Commit

--- 0-starter/src/index.jsx
+++ 0-starter/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <h1>This is my first React App</h1>,
+    <App />,
     document.getElementById('app')
 );
